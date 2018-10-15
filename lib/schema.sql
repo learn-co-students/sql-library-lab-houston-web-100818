d065a56e47bfaf5e3c386c2032b5dbb3d3d80132
Alexander Carlin <ahcarlin@gmail.com>
2018-10-15 23:39:50 +0000

    Done.

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,47 +1,31 @@
-create database library;
-
-create table series(
-  id INTEGER PRIMARY KEY,
-  title TEXT,
-  author_id INTEGER,
-  subgenre_id INTEGER,
-  FOREIGN KEY(author_id) REFERENCES(authors.id),
-  FOREIGN KEY(subgenre_id) REFERENCES(subgenre.id)
+CREATE TABLE series (id INTEGER PRIMARY KEY,
+title TEXT,
+author_id INTEGER,
+subgenre_id INTEGER
 );
 
-create table subgenre(
-  id INTEGER PRIMARY KEY,
-  name TEXT
+CREATE TABLE subgenres (id INTEGER PRIMARY KEY,
+name TEXT
 );
 
-create table authors(
-  id INTEGER PRIMARY KEY,
-  name TEXT
+CREATE TABLE authors (id INTEGER PRIMARY KEY,
+name TEXT
 );
 
-create table books(
-  id INTEGER PRIMARY KEY,
-  title TEXT,
-  year INTEGER,
-  series_id INTEGER,
-  FOREIGN KEY(series_id) REFERENCES(series.id)
+CREATE TABLE books (id INTEGER PRIMARY KEY,
+title TEXT,
+year INTEGER,
+series_id INTEGER
 );
 
-create table characters(
-  id INTEGER PRIMARY KEY,
-  name TEXT, 
-  motto TEXT,
-  species TEXT, 
-  author_id INTEGER,
-  series_id INTEGER,
-  FOREIGN KEY(author_id) REFERENCES(authors.id),
-  FOREIGN KEY(series_id) REFERENCES(series.id)
+CREATE TABLE characters (id INTEGER PRIMARY KEY,
+name TEXT,
+motto TEXT,
+species TEXT,
+author_id INTEGER,
+series_id INTEGER
 );
 
-create table character_books(
-  id INTEGER PRIMARY KEY,
-  character_id INTEGER,
-  book_id INTEGER,
-  FOREIGN KEY(character_id) REFERENCES(characters.id),
-  FOREIGN KEY(book_id) REFERENCES(books.id)
-);
+CREATE TABLE character_books (id INTEGER PRIMARY KEY,
+character_id INTEGER,
+book_id INTEGER);
